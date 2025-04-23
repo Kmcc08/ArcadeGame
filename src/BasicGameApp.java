1bb45c0f6da874827745cc0b160b23e430d0958b
kmcc08 <kat_mccormick27@milton.edu>
2025-04-23 00:27:48 -0400

    Initial commit

---
+++ src/BasicGameApp.java
@@ -0,0 +1,254 @@
+//Basic Game Application
+//Version 2
+// Basic Object, Image, Movement
+// basketnaut moves to the right.
+// Threaded
+
+//K. Chun 8/2018
+
+//*******************************************************************************
+//Import Section
+//Add Java libraries needed for the game
+//import java.awt.Canvas;
+
+//Graphics Libraries
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+//*******************************************************************************
+// Class Definition Section
+
+// step 1 add KeyListener to the implements
+public class BasicGameApp implements Runnable, KeyListener {
+
+	//Variable Definition Section
+	//Declare the variables used in the program
+	//You can set their initial values too
+
+	//Sets the width and height of the program window
+	final int WIDTH = 1000;
+	final int HEIGHT = 700;
+
+	//Declare the variables needed for the graphics
+	public JFrame frame;
+	public Canvas canvas;
+	public JPanel panel;
+
+	public BufferStrategy bufferStrategy;
+	public Image basketPic;
+
+	//Declare the objects used in the program
+	//These are things that are made up of more than one variable type
+	private Basket basket;
+	private Basket apple;
+	private Basket appleTrees;
+	Basket[]  basketArray = new Basket[10];
+	private Image appleTreesPic;
+	private Image applePic;
+
+
+	// Main method definition
+	// This is the code that runs first and automatically
+	public static void main(String[] args) {
+		BasicGameApp ex = new BasicGameApp();   //creates a new instance of the game
+		new Thread(ex).start();                 //creates a threads & starts up the code in the run( ) method
+	}
+
+
+	// Constructor Method
+	// This has the same name as the class
+	// This section is the setup portion of the program
+	// Initialize your variables and construct your program objects here.
+	public BasicGameApp() {
+
+		setUpGraphics();
+
+		//variable and objects
+		//create (construct) the objects needed for the game and load up
+		basketPic = Toolkit.getDefaultToolkit().getImage("basket.jpg");
+		applePic = Toolkit.getDefaultToolkit().getImage("apple.jpg");
+		appleTreesPic = Toolkit.getDefaultToolkit().getImage("apple trees.jpg");//load the picture
+		basket = new Basket(10,100);
+		appleTrees = new Basket(600,600);
+		apple = new Basket(600,600);
+
+		for(int x = 0; x<basketArray.length;x++){
+			basketArray[x] = new Basket((int)Math.random()*900,(int)(Math.random()*600));
+		}
+
+
+	}// BasicGameApp()
+
+
+//*******************************************************************************
+//User Method Section
+//
+// put your code to do things here.
+
+	// main thread
+	// this is the code that plays the game after you set things up
+	public void run() {
+
+		//for the moment we will loop things forever.
+		while (true) {
+
+			moveThings();  //move all the game objects
+			render();  // paint the graphics
+			pause(20); // sleep for 10 ms
+		}
+	}
+
+
+	public void moveThings()
+	{
+		//calls the move( ) code in the objects
+		basket.move();
+		basket.wrap();
+		collisions();
+		for(int y=0; y< basketArray.length; y++){
+			basketArray[y].move();
+		}
+
+
+	}
+	public void collisions(){
+
+
+
+		for(int b = 0; b< basketArray.length; b++){
+			if(basket.rec.intersects(basketArray[b].rec)){
+				System.out.println("crashing");
+			}
+		}
+
+	}
+
+	//Pauses or sleeps the computer for the amount specified in milliseconds
+	public void pause(int time ){
+		//sleep
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+
+		}
+	}
+
+
+	//Graphics setup method
+	private void setUpGraphics() {
+		frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
+
+		panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
+		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
+		panel.setLayout(null);   //set the layout
+
+		// creates a canvas which is a blank rectangular area of the screen onto which the application can draw
+		// and trap input events (Mouse and Keyboard events)
+		canvas = new Canvas();
+		canvas.setBounds(0, 0, WIDTH, HEIGHT);
+		canvas.setIgnoreRepaint(true);
+// step 2 add keylistner to canvas as this
+		canvas.addKeyListener(this);
+
+		panel.add(canvas);  // adds the canvas to the panel.
+
+		// frame operations
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
+		frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
+		frame.setResizable(false);   //makes it so the frame cannot be resized
+		frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
+
+		// sets up things so the screen displays images nicely.
+		canvas.createBufferStrategy(2);
+		bufferStrategy = canvas.getBufferStrategy();
+		canvas.requestFocus();
+		System.out.println("DONE graphic setup");
+
+	}
+
+
+	//paints things on the screen using bufferStrategy
+	private void render() {
+		Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+		g.clearRect(0, 0, WIDTH, HEIGHT);
+
+		//draw the image of the basketnaut
+		g.drawImage(basketPic, basket.xpos, basket.ypos, basket.width, basket.height, null);
+		g.drawImage(applePic, apple.xpos, apple.ypos, apple.width, apple.height, null);
+		g.drawImage(appleTreesPic, appleTrees.xpos, appleTrees.ypos, appleTrees.width, appleTrees.height, null);
+		for(int l = 0; l < basketArray.length; l++){
+			g.drawImage(basketPic, basketArray[l].xpos, basketArray[l].ypos, basketArray[l].width, basketArray[l].height, null);
+		}
+		g.dispose();
+
+		bufferStrategy.show();
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.out.println(e.getKeyChar());
+		System.out.println(e.getKeyCode());
+		if (e.getKeyCode() == 38){
+
+			basket.dy = -5;
+			basket.dx = 0;
+		}
+		if (e.getKeyCode() == 40){
+
+			basket.dy = 5;
+			basket.dx = 0;
+		}
+		if (e.getKeyCode() == 39){;
+
+			basket.dx = 5;
+			basket.dy = 0;
+
+		}
+		if (e.getKeyCode() == 37){
+
+			basket.dx = -5;
+			basket.dy = 0;
+		}
+		if(e.getKeyCode() == 16){
+
+			basket.dy = -5;
+			basket.dx =-5;
+		}
+	}
+
+	// hw: identify key codes for up down left and right arrow keys
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+		System.out.println(e.getKeyChar());
+		System.out.println(e.getKeyCode());
+		if(e.getKeyCode() == 38){
+			basket.up = false;
+		}
+		if(e.getKeyCode() == 40){
+			basket.down = false;
+		}
+		if(e.getKeyCode() == 37){
+			basket.left = false;
+		}
+		if(e.getKeyCode() == 38){
+			basket.right = false;
+		}
+
+
+	}
+
+	// ad step 3 add methods keyReleased, keyPressed, and keyType
+}
---
+++ src/Basket.java
@@ -0,0 +1,143 @@
+import java.awt.*;
+
+/**
+ * Created by chales on 11/6/2017.
+ */
+public class Basket {
+
+    //VARIABLE DECLARATION SECTION
+    //Here's where you state which variables you are going to use.
+    public String name;                //holds the name of the hero
+    public int xpos;                //the x position
+    public int ypos;                //the y position
+    public int dx;                    //the speed of the hero in the x direction
+    public int dy;                    //the speed of the hero in the y direction
+    public int width;
+    public int height;
+    public boolean isAlive;
+    public Rectangle rec;
+    public boolean up;
+    public boolean down;
+    public boolean left;
+    public boolean right;
+
+
+    //a boolean to denote if the hero is alive or dead.
+
+
+    // METHOD DEFINITION SECTION
+
+    // Constructor Definition
+    // A constructor builds the object when called and sets variable values.
+
+
+    //This is a SECOND constructor that takes 3 parameters.  This allows us to specify the hero's name and position when we build it.
+    // if you put in a String, an int and an int the program will use this constructor instead of the one above.
+    public Basket(int pXpos, int pYpos) {
+        xpos = pXpos;
+        ypos = pYpos;
+        dx =2;
+        dy =4;
+
+        width = 60;
+        height = 60;
+        isAlive = true;
+        left =false;
+        up = false;
+        down = false;
+        right = false;
+        rec=new Rectangle(xpos,ypos,width,height);
+
+    } // constructor
+
+    public void wrap(){  //this method causes one character to wrap by "going through the walls" of the screen instead of bouncing
+
+        if(up==true){
+            dy = -5;
+        }
+        if(down==true){
+            dy=5;
+        }
+        if(left == true){
+            dx=-5;
+        }
+        if(right==true){
+            dx = 5;
+        }
+        if(xpos>1000) {
+            xpos = 0;
+
+        }
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+
+        if(xpos<0){
+            xpos = 1000;
+            xpos = xpos + dx;
+            ypos = ypos + dy;
+        }
+
+        if(ypos>700){
+            ypos = 0;
+            xpos = xpos + dx;
+            ypos = ypos + dy;
+        }
+
+        if(ypos<0){
+            ypos = 700;
+            xpos = xpos + dx;
+            ypos = ypos + dy;
+        }
+        rec  = new Rectangle(xpos, ypos, width, height);
+
+
+    }
+
+    //The move method.  Everytime this is run (or "called") the hero's x position and y position change by dx and dy
+    public void move() {
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+        if(up ==true){
+            dy = -5;
+        }
+        if(down == true){
+            dy = 5;
+        }
+        if(left == true){
+            dx = -5;
+        }
+        if(right == true){
+
+        }
+        rec=new Rectangle(xpos,ypos,width,height);
+
+    }
+    public void bounce (){ //this method causes them to switch directions when they intersect
+        if (xpos>1000) {
+            dx=-dx;
+        }
+
+        if (xpos<0) {
+            dx=-dx;
+        }
+        if (ypos>600) {
+            dy=-dy;
+        }
+
+        if (ypos<0) {
+            dy=-dy;
+        }
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+
+        rec  = new Rectangle(xpos, ypos, width, height);
+
+    }
+
+}
+
+
+
+
+
+
